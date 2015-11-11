ded56f7c350f757b77e0ffb506e48f84210294e3
yessika21289 <yessika.naftali@reebonz.com>
2015-11-12 00:30:53 +0700

    Fixing tinyMCE format.

--- application/views/content_admin_header.php
+++ application/views/content_admin_header.php
@@ -22,13 +22,31 @@
         tinymce.init({
             selector: "#editor",
             plugins: [
-                "print preview ",
-                "searchreplace visualblocks fullscreen",
-                "contextmenu paste"
+                "advlist autolink autosave link image lists charmap print preview hr anchor pagebreak spellchecker",
+                "searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking",
+                "table contextmenu directionality emoticons template textcolor paste fullpage textcolor colorpicker textpattern"
             ],
-            toolbar: "undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link"
-        });
-    </script>
+
+            toolbar1: "bold italic underline strikethrough subscript superscript | alignleft aligncenter alignright alignjustify | bullist numlist | outdent indent blockquote | undo redo | link unlink",
+
+            menubar: false,
+            toolbar_items_size: 'small',
+
+            style_formats: [
+                {title: 'Bold text', inline: 'b'},
+                {title: 'Red text', inline: 'span', styles: {color: '#ff0000'}},
+                {title: 'Red header', block: 'h1', styles: {color: '#ff0000'}},
+                {title: 'Example 1', inline: 'span', classes: 'example1'},
+                {title: 'Example 2', inline: 'span', classes: 'example2'},
+                {title: 'Table styles'},
+                {title: 'Table row 1', selector: 'tr', classes: 'tablerow1'}
+            ],
+
+            templates: [
+                {title: 'Test template 1', content: 'Test 1'},
+                {title: 'Test template 2', content: 'Test 2'}
+            ]
+        });</script>
 </head>
 <body>
 	
