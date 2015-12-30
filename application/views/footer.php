8f1bd5fd9dded5c7458687420e5e01bf582920dd
Yessika <yessika21289@gmail.com>
2015-12-30 07:37:46 +0700

    [#43] 1. Kontak di footer sesuaikan dengan instansi yang sedang tampil saat itu

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,7 +1,16 @@
+<?php
+	$this->load->model('ypki');
+	$kontak = $this->ypki->getKontak($instansi);
+?>
+<?php if(!empty($kontak)): ?>
+	<footer>
+		TELEPON KANTOR <?php echo strtoupper($instansi); ?> MAGELANG : <?php echo $kontak[0]->telepon1; ?> | EMAIL : <?php echo $kontak[0]->email; ?>
+	</footer>
+<?php else: ?>
 	<footer>
 		TELEPON KANTOR YPKI MAGELANG : (0293) 5573950 | EMAIL : SEKRETARIAT@YPKI.OR.ID
 	</footer>
-
+<?php endif; ?>
 </div>
 
 </body>
